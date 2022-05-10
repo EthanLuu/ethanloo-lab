72664c7cbe4913c8de908a3631a6bb10452b316f
EthanLuu <ethanlooo@outlook.com>
2022-05-10 11:55:48 +0800

    ✨ feat: add beian

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,6 +2,9 @@
     import "../app.css";
 </script>
 
-<main class="min-h-screen text-slate-200 bg-cover flex">
+<main class="min-h-screen text-slate-200 bg-cover flex flex-col">
     <slot />
+    <footer class="absolute bottom-0 w-full text-center py-2 font-serif">
+        <a href="https://beian.miit.gov.cn/" class="text-sm`"> 苏ICP备2020057383号-2 </a>
+    </footer>
 </main>
